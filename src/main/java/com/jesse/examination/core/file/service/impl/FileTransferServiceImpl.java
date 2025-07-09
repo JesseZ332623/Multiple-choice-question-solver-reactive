00d994ced09859e4d22aa756a4d0f657f48a9710
浩进 朱 <3191955858@qq.com>
2025-07-09 21:44:24 +0800

    文件操作核心接口实现类。

--- src/main/java/com/jesse/examination/core/file/service/impl/FileTransferServiceImpl.java
+++ src/main/java/com/jesse/examination/core/file/service/impl/FileTransferServiceImpl.java
@@ -10,18 +10,20 @@ import reactor.core.scheduler.Schedulers;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 import java.nio.file.StandardOpenOption;
 import java.util.Objects;
 
 import static java.lang.String.format;
 
+/** 文件操作核心方法实现类。 */
 @Slf4j
 @Component
 public class FileTransferServiceImpl implements FileTransferService
 {
     /** 文件操作异常信息模板。 */
     private static final String ERROR_MESSAGE_TEMPLATE
-        = "File operator failed! (File name: [%s], Cause: %s.)";
+        = "File operator failed! (File: [%s], Cause: %s)";
 
     /**
      * 检查传入的文件路径是否存在，不存在则创建之。
@@ -37,7 +39,8 @@ public class FileTransferServiceImpl implements FileTransferService
     }
 
     /** 提取文件的扩展名并转成大写。 */
-    private static String getFileExtension(String fileName) {
+    private static String getFileExtension(String fileName)
+    {
         int dotIndex = fileName.lastIndexOf(".");
 
         return (dotIndex == -1)
@@ -119,5 +122,64 @@ public class FileTransferServiceImpl implements FileTransferService
                         fileName, exception.getMessage()
                     ), exception
                 ))).then();
+    }
+
+    @Override
+    public Mono<Void>
+    renameDirectory(Path oldPathName, Path newPathName)
+    {
+        Objects.requireNonNull(oldPathName, "Old file path path cannot be null");
+        Objects.requireNonNull(newPathName, "New file path name cannot be null");
+
+        return Mono.fromCallable(
+            () -> {
+                if (!Files.exists(oldPathName))
+                {
+                    throw new IllegalArgumentException(
+                        format("source path: %s not exist!", oldPathName)
+                    );
+                }
+
+                Files.createDirectories(newPathName.getParent());
+
+                /*
+                 * 新旧路径校验完后，进行文件的移动操作，Files.move() 方法在不同情况下的行为也不同：
+                 *
+                 *  case 1：若同盘移动则仅修改文件的元数据，速度极快。
+                 *  case 2: 若跨盘移动则触发文件的复制，在目录层级很深的时候有性能问题。
+                 *
+                 * 这里还需要解释下面两个参数：
+                 *
+                 * 1. StandardCopyOption.REPLACE_EXISTING
+                 *      在操作路径时，若目标路径已经存在，则直接抛出 FileAlreadyExistsException
+                 *
+                 * 2. StandardCopyOption.ATOMIC_MOVE
+                 *      将该移动操作视为一个整体（原子化），如果期间出现任何失败，则都会回滚操作。
+                 *
+                 * 但当前项目的对该方法的使用不涉及跨盘的操作，所以不需要 REPLACE_EXISTING 参数。
+                 */
+                Files.move(
+                    oldPathName, newPathName,
+                    StandardCopyOption.ATOMIC_MOVE
+                );
+
+                return null;
+            }
+        ).subscribeOn(Schedulers.boundedElastic())
+        .doOnError((exception) ->
+            log.error(
+            "Rename directory {} -> {} failed!",
+            oldPathName, newPathName, exception
+        ))
+        .onErrorResume((exception) ->
+            Mono.error(
+                new FileOperatorException(
+                    format(
+                        ERROR_MESSAGE_TEMPLATE,
+                        format("%s -> %s", oldPathName, newPathName),
+                        exception.getMessage()
+                    ), exception
+            )
+        )).then();
     }
 }
