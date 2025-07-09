8af273d0e94d43bcc88eed1b62567fc223b82e01
浩进 朱 <3191955858@qq.com>
2025-07-09 21:44:40 +0800

    包含一个问题的所有信息的 DTO。

---
+++ src/main/java/com/jesse/examination/question/dto/FullQuestionInfoDTO.java
@@ -0,0 +1,20 @@
+package com.jesse.examination.question.dto;
+import com.jesse.examination.question.entity.Option.AnswerOption;
+import lombok.*;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+/** 包含一个问题的所有信息的 DTO。 */
+@Data
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class FullQuestionInfoDTO
+{
+    private Integer                   questionId;
+    private String                    questionContent;
+    private AnswerOption              correctAnswer;
+    private Map<AnswerOption, String> options = new TreeMap<>();
+}
