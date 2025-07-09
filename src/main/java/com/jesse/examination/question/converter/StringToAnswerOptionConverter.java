90412d15a079354326caca222803ea4a6678771a
浩进 朱 <3191955858@qq.com>
2025-07-09 21:53:29 +0800

    将字符串转化成 AnswerOption 枚举类的转换器。

---
+++ src/main/java/com/jesse/examination/question/converter/StringToAnswerOptionConverter.java
@@ -0,0 +1,14 @@
+package com.jesse.examination.question.converter;
+
+import com.jesse.examination.question.entity.Option.AnswerOption;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.core.convert.converter.Converter;
+
+/** 将字符串转化成 AnswerOption 枚举类的转换器。*/
+public class StringToAnswerOptionConverter implements Converter<String, AnswerOption>
+{
+    @Override
+    public AnswerOption convert(@NotNull String source) {
+        return AnswerOption.valueOf(source);
+    }
+}
