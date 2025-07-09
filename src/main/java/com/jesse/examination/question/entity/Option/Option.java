21c26050a3437b4e8528c4a9314fa64ad1fedf29
浩进 朱 <3191955858@qq.com>
2025-07-09 22:00:42 +0800

    问题选项数据表实体类。

---
+++ src/main/java/com/jesse/examination/question/entity/Option/Option.java
@@ -0,0 +1,32 @@
+package com.jesse.examination.question.entity.Option;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+/** 问题选项数据表实体类。 */
+@Data
+@ToString
+@Table(name = "options")
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
+@AllArgsConstructor
+public class Option
+{
+    /**
+     * 哑 ID，没有别的意义，只是因为实体必须要有一个 @Id 注解。
+     * R2DBC 没有 JPA 那般灵活，
+     * 对复合主键的支持有限，所以这块需要手动操作。
+     */
+    @Id
+    private Long dummyId = -1L;
+
+    @Column("question_id")  // 显式映射 question_id
+    private Integer questionId;
+
+    @Column("option_key")   // 显式映射 option_key
+    private AnswerOption optionKey;
+
+    @Column(value = "content")
+    private String content;
+}
