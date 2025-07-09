c54b6a84283fd7a1b2c14ef777e6b2293212137e
浩进 朱 <3191955858@qq.com>
2025-07-09 21:47:02 +0800

    问题选项数据表仓储类。

---
+++ src/main/java/com/jesse/examination/question/repository/OptionRepository.java
@@ -0,0 +1,49 @@
+package com.jesse.examination.question.repository;
+
+import com.jesse.examination.question.entity.Option.AnswerOption;
+import com.jesse.examination.question.entity.Option.Option;
+import org.springframework.data.r2dbc.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public interface OptionRepository
+    extends ReactiveCrudRepository<Option, Long>
+{
+    /**
+     * 根据问题的 ID 获取问题的所有选项。
+     *
+     * @param questionId 问题 ID
+     *
+     * @return 承载了这个问题所有选项的 Flux
+     */
+    @Query(value = """
+            SELECT question_id, option_key, content
+            FROM exam_question.options
+            WHERE question_id = :questionId
+        """)
+    Flux<Option> findByQuestionId(
+        @Param(value = "questionId") Integer questionId
+    );
+
+    /**
+     * 通过复合主键去查询某个问题的某一条选项。
+     *
+     * @param questionId 问题 ID
+     * @param option     选项号
+     *
+     * @return 承载了执行问题指定选项的 Mono
+     */
+    @Query(value = """
+            SELECT question_id, option_key, content
+            FROM exam_question.options
+            WHERE question_id = :questionId
+                  AND
+                  option_key = :option
+        """)
+    Mono<Option> findOneOptionById(
+        @Param(value = "questionId") Integer questionId,
+        @Param(value = "option")     AnswerOption option
+    );
+}
