6149f2bec24b84c745061044e80b88b39b0ad8d5
浩进 朱 <3191955858@qq.com>
2025-07-10 15:03:57 +0800

    问题数据表仓储类。

--- src/main/java/com/jesse/examination/question/repository/QuestionRepository.java
+++ src/main/java/com/jesse/examination/question/repository/QuestionRepository.java
@@ -1,25 +1,68 @@
 package com.jesse.examination.question.repository;
 
 import com.jesse.examination.question.dto.FullQuestionInfoDTO;
+import com.jesse.examination.question.dto.QuestionWithCorrectDTO;
 import com.jesse.examination.question.entity.Question.Question;
 import org.springframework.data.r2dbc.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 public interface QuestionRepository
     extends ReactiveCrudRepository<Question, Integer>
 {
     /**
-     * <p>获取所有问题的 id，内容，正确答案，所有选项。示例如下：</p>
+     * 根据问题 ID 查询某一条完整的问题。
      *
-     * <code>
-     *      <pre>
-     * QuestionID: 1
-     * QuestionContent: 若某机器数为10000000，它代表-127，则它是
-     * CorrectAnswer: B
-     * OptionsJSON: {"A": "原码", "B": "反码", "C": "补码", "D": "移码"}
-     *      </pre>
-     * </code>
+     * @param questionId 问题 ID
+     *
+     * @return 承载了单条问题完整信息的 Mono
+     */
+    @Query(
+        value = """
+            SELECT
+                questions.id        AS question_id,
+                questions.content   AS question_content,
+                questions.answer    AS correct_answer,
+                JSON_OBJECTAGG(options.option_key, options.content) AS options
+            FROM
+                questions
+            INNER JOIN
+                options ON questions.id = options.question_id
+            WHERE question_id = :questionId
+            GROUP BY questions.id
+            """
+    )
+    Mono<FullQuestionInfoDTO>
+    findOneQuestionWithAllOptions(
+        @Param(value = "questionId") Integer questionId
+    );
+
+    /**
+     * 分页的获取所有问题的 ID，内容，正确选项，正确答案。
+     */
+    @Query(value = """
+        SELECT
+            questions.id      AS question_id,
+            questions.content AS question_content,
+            questions.answer  AS correct_answer,
+            options.content   AS answer_content
+        FROM questions
+        INNER JOIN options ON questions.id = options.question_id
+        WHERE options.option_key = questions.answer
+        GROUP BY questions.id
+        LIMIT :limit OFFSET :offset
+        """
+    )
+    Flux<QuestionWithCorrectDTO>
+    findAllQuestionWithCorrectAnswer(
+        @Param(value = "limit")  int limit,
+        @Param(value = "offset") int offset
+    );
+
+    /**
+     * 分页的获取所有问题的 ID，内容，正确选项，所有选项和内容。
      */
     @Query(
         value = """
@@ -33,7 +76,12 @@ public interface QuestionRepository
             INNER JOIN
                 options ON questions.id = options.question_id
             GROUP BY questions.id
+            LIMIT :limit OFFSET :offset
             """
     )
-    Flux<FullQuestionInfoDTO> findQuestionWithAllOptions();
+    Flux<FullQuestionInfoDTO>
+    findAllQuestionWithAllOptions(
+        @Param(value = "limit")  int limit,
+        @Param(value = "offset") int offset
+    );
 }
