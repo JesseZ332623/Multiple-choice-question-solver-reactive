d77d5018b765186b001615d6cdfe0be2859f0f3f
浩进 朱 <3191955858@qq.com>
2025-07-14 16:21:08 +0800

    用户模块 Redis 服务接口类。

---
+++ src/main/java/com/jesse/examination/user/redis/UserRedisService.java
@@ -0,0 +1,52 @@
+package com.jesse.examination.user.redis;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.Map;
+
+/** 用户模块 Redis 服务接口类。 */
+public interface UserRedisService
+{
+    /**
+     * 某用户登录时，
+     * 将从该用户存档中读取所有问题答对次数数据以哈希表的形式存入 Redis。
+     *
+     * @param userName 用户名
+     * @param quesCorrectTimesMap
+     *        从 JSON 存档文件中读出，并映射后的哈希表，
+     *        映射关系是：问题 ID -> 问题答对次数
+     *
+     * @return 是否成功存入 Redis?
+     */
+    Mono<Boolean>
+    loadUserQuestionCorrectTimes(
+        String userName,
+        Map<String, Long> quesCorrectTimesMap
+    );
+
+    /**
+     * 某用户登出时，
+     * 将从 Redis 中读取所有问题答对次数数据哈希表，再存回用户存档中。
+     *
+     * @param userName 用户名
+     *
+     * @return 从 Redis 中读取的所有问题答对次数数据哈希表
+     */
+    Mono<Map<String, Long>>
+    getUserQuestionCorrectTimes(String userName);
+
+    /**
+     * 用户在登出且数据存回存档后，
+     * 需要删除他在 Redis 中的所有信息。
+     */
+    Mono<Boolean>
+    deleteUserInfo(String userName);
+
+    /**
+     * 获取所有的用户名。
+     *
+     * @return 承载了所有用户名的 Flux
+     */
+    Flux<String>  getAllUsers();
+}
