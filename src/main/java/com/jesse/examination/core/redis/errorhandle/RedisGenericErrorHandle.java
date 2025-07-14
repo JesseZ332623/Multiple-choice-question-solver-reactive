ea80d009e5f4183da2b71a180fbee0f47f6d152f
浩进 朱 <3191955858@qq.com>
2025-07-14 16:14:46 +0800

    本项目所有的 Redis 操作中，通用的错误处理方法。

---
+++ src/main/java/com/jesse/examination/core/redis/errorhandle/RedisGenericErrorHandle.java
@@ -0,0 +1,55 @@
+package com.jesse.examination.core.redis.errorhandle;
+
+import io.lettuce.core.RedisCommandTimeoutException;
+import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.data.redis.RedisConnectionFailureException;
+import org.springframework.data.redis.serializer.SerializationException;
+import reactor.core.publisher.Mono;
+
+/** 本项目所有的 Redis 操作中，通用的错误处理方法。 */
+@Slf4j
+public class RedisGenericErrorHandle
+{
+    /**
+     * 本项目所有的 Redis 操作中，通用的错误处理方法。
+     *
+     * @param <T> 承载数据类型
+     *
+     * @param exception     Redis 操作中可能抛出的异常
+     * @param fallbackValue 出错后可能需要返回的默认值
+     *
+     * @return 承载了异常或者默认值的 Mono
+     */
+    public static <T> @NotNull Mono<T>
+    genericErrorHandel(@NotNull Throwable exception, T fallbackValue)
+    {
+        switch (exception)
+        {
+            case RedisConnectionFailureException redisConnectionFailureException ->
+                log.error(
+                    "Redis connect failed! Cause: {}",
+                    redisConnectionFailureException.toString()
+                );
+            case RedisCommandTimeoutException redisCommandTimeoutException ->
+                log.warn(
+                    "Redis operator time out! Cause: {}",
+                    redisCommandTimeoutException.toString()
+                );
+            case SerializationException serializationException ->
+                log.error(
+                    "Data deserialization failed! Cause: {}",
+                    serializationException.toString()
+                );
+            default ->
+                log.error(
+                    "Redis operator exception! Cause: {}",
+                    exception.toString()
+                );
+        }
+
+        /* 若 fallbackValue 的值为空，异常会向上传递。*/
+        return (fallbackValue != null)
+            ? Mono.just(fallbackValue) : Mono.error(exception);
+    }
+}
