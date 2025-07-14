dd97a1b85b1d25c5eac002b2f6b946b1b8a81f94
浩进 朱 <3191955858@qq.com>
2025-07-14 18:06:31 +0800

    问题查询模块路由函数配置类。

--- src/main/java/com/jesse/examination/question/route/QuestionRouteConfig.java
+++ src/main/java/com/jesse/examination/question/route/QuestionRouteConfig.java
@@ -1,11 +1,9 @@
 package com.jesse.examination.question.route;
 
 import com.jesse.examination.question.service.QuestionService;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.config.EnableWebFlux;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
@@ -13,9 +11,7 @@ import org.springframework.web.reactive.function.server.ServerResponse;
 import static com.jesse.examination.question.route.QuestionServiceURL.*;
 
 /** 问题查询模块路由函数配置类。 */
-@Slf4j
 @Configuration
-@EnableWebFlux
 public class QuestionRouteConfig
 {
     @Autowired
