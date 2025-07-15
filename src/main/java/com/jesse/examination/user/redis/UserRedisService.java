49e683f4fcde834c6570ab9f48871d25c8d27d11
浩进 朱 <3191955858@qq.com>
2025-07-15 14:16:55 +0800

    用户模块 Redis 服务接口类。（注释补全）

--- src/main/java/com/jesse/examination/user/redis/UserRedisService.java
+++ src/main/java/com/jesse/examination/user/redis/UserRedisService.java
@@ -1,6 +1,5 @@
 package com.jesse.examination.user.redis;
 
-import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -10,8 +9,35 @@ import java.util.Map;
 public interface UserRedisService
 {
     /**
-     * 某用户登录时，
-     * 将从该用户存档中读取所有问题答对次数数据以哈希表的形式存入 Redis。
+     * <p>
+     *     用户登录时，发送验证码邮件之后，存储验证码到 Redis。
+     *     键值对格式如下：
+     * </p>
+     *
+     * <code><pre>
+     * [Key]    user:[userName]:varify-code
+     * [Value]  (Numeric String)
+     * </pre></code>
+     *
+     * @param userName   用户名
+     * @param varifyCode 验证码
+     *
+     * @return 是否成功存入 Redis?
+     */
+    Mono<Boolean>
+    saveUserVarifyCode(String userName, String varifyCode);
+
+    /**
+     * <p>
+     *     某用户登录时，
+     *     将从该用户存档中读取所有问题答对次数数据以哈希表的形式存入 Redis。
+     *     键值对格式如下：
+     * </p>
+     *
+     * <code><pre>
+     * [Key]    user:[userName]:ques-correct-times
+     * [Value]  (Hash Map)
+     * </pre></code>
      *
      * @param userName 用户名
      * @param quesCorrectTimesMap
@@ -27,8 +53,31 @@ public interface UserRedisService
     );
 
     /**
-     * 某用户登出时，
-     * 将从 Redis 中读取所有问题答对次数数据哈希表，再存回用户存档中。
+     * <p>
+     *     读取指定用户的验证码。
+     * </p>
+     *
+     * <code><pre>
+     * [Key]    user:[userName]:varify-code
+     * </pre></code>
+     *
+     * @param userName   用户名
+     *
+     * @return 承载了指定用户验证码数据的 Mono
+     */
+    Mono<String>
+    getUserVarifyCode(String userName);
+
+    /**
+     * <p>
+     *     某用户登出时，
+     *     将从 Redis 中读取所有问题答对次数数据哈希表，再存回用户存档中。
+     *     键格式如下：
+     * </p>
+     *
+     * <code><pre>
+     * [Key]    user:[userName]:ques-correct-times
+     * </pre></code>
      *
      * @param userName 用户名
      *
@@ -38,8 +87,11 @@ public interface UserRedisService
     getUserQuestionCorrectTimes(String userName);
 
     /**
-     * 用户在登出且数据存回存档后，
-     * 需要删除他在 Redis 中的所有信息。
+     * 将某个用户下的所有数据删除。
+     *
+     * @param userName 用户名
+     *
+     * @return 是否删除成功？
      */
     Mono<Boolean>
     deleteUserInfo(String userName);
@@ -50,10 +102,4 @@ public interface UserRedisService
      * @return 承载了所有用户名的 Flux
      */
     Flux<String>  getAllUsers();
-
-    /** 获取响应式 Redis 模板，
-     *  测试时用（生产环境拒绝这种破坏封装性的行为）。
-     */
-    ReactiveRedisTemplate<String, Object>
-    getRedisTemplate();
 }
