23d6685a09c3d84228f6ed03db64de8f08e15b01
Jesse <3191955858@qq.com>
2025-07-19 19:41:29 +0800

    邮件验证码发送测试类。

--- src/test/java/com/jesse/examination/core/EmailSendTest.java
+++ src/test/java/com/jesse/examination/core/EmailSendTest.java
@@ -4,6 +4,7 @@ import com.jesse.examination.core.email.dto.EmailContent;
 import com.jesse.examination.core.email.service.EmailAuthQueryService;
 import com.jesse.examination.core.email.service.EmailSenderInterface;
 import com.jesse.examination.core.email.utils.VarifyCodeGenerator;
+import com.jesse.examination.core.properties.ProjectProperties;
 import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.NotNull;
@@ -15,14 +16,14 @@ import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
+import java.time.Duration;
 import java.util.AbstractMap;
 
 import static com.jesse.examination.core.redis.keys.ProjectRedisKey.ENTERPRISE_EMAIL_ADDRESS;
 import static com.jesse.examination.core.redis.keys.ProjectRedisKey.SERVICE_AUTH_CODE;
 import static com.jesse.examination.core.logmakers.LogMakers.EMAIL_SENDER;
-import static java.lang.String.format;
 
-/** 邮件验证码发送测试类。 */
+/** 邮件验证码发送测试类。*/
 @Slf4j
 @SpringBootTest
 public class EmailSendTest
@@ -36,6 +37,9 @@ public class EmailSendTest
     @Autowired
     @Qualifier(value = "createEmailSender")
     private EmailSenderInterface emailSender;
+
+    @Autowired
+    private ProjectProperties projectProperties;
 
     /**
      * 在所以依赖注入都完成后，
@@ -63,45 +67,27 @@ public class EmailSendTest
             );
 
         StepVerifier.create(readPublisherInfo)
-            .expectNext(true)
-            .verifyComplete();
-    }
-
-    private @NotNull Mono<EmailContent>
-    getEmailContentDTO(String userName, String userEmail, String varifyCode)
-    {
-        EmailContent emailContent = new EmailContent();
-
-        emailContent.setTo(userEmail);
-        emailContent.setSubject("用户：" + userName + " 请查收您的验证码。");
-        emailContent.setTextBody(
-            format(
-                "用户：%s 您的验证码是：[%s]，" +
-                    "请在 %d 分钟内完成验证，超过 %d 分钟后验证码自动失效！",
-                userName, varifyCode, 3, 3
-            )
-        );
-
-        // 暂时没有附件
-        emailContent.setAttachmentPath(null);
-
-        return Mono.just(emailContent);
+                    .expectNext(true)
+                    .verifyComplete();
     }
 
     @Test
     public void EmailSenderTest()
     {
         Mono<AbstractMap.SimpleEntry<Boolean, String>> sendEmailStream
-            = VarifyCodeGenerator.generateVarifyCode(8).flatMap(
-            (code) -> {
+            = VarifyCodeGenerator.generateVarifyCode(
+                Integer.parseInt(this.projectProperties.getVarifyCodeLength())
+            ).flatMap((code) -> {
                 // 尝试发送邮件
-                return this.getEmailContentDTO(
-                        "Peter",
-                        "zhj3191955858@gmail.com",
-                        code
-                    ).flatMap(
-                        (content) ->
-                            this.emailSender.sendEmail(content)
+                return this.emailSender
+                           .sendEmail(EmailContent.fromVarify(
+                               "Jesse",
+                               "zhj3191955858@gmail.com",
+                               code,
+                               Duration.ofMinutes(
+                                    Long.parseLong(this.projectProperties.getVarifyCodeExpiration()) / 60
+                               )
+                           )
                     )
                     .doOnSuccess((res) ->
                         log.info(
