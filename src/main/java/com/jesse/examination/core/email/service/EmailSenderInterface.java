115101bcf56b8b95cc1aff627e5b9793eb819551
Jesse <3191955858@qq.com>
2025-07-19 19:41:12 +0800

    封装了 javax.mail 库的响应式邮件发送器接口。

--- src/main/java/com/jesse/examination/core/email/service/EmailSenderInterface.java
+++ src/main/java/com/jesse/examination/core/email/service/EmailSenderInterface.java
@@ -6,7 +6,7 @@ import reactor.core.publisher.Mono;
 
 import java.util.AbstractMap;
 
-/**  */
+/** 封装了 javax.mail 库的响应式邮件发送器接口。*/
 public interface EmailSenderInterface
 {
     Mono<AbstractMap.SimpleEntry<Boolean, String>>
