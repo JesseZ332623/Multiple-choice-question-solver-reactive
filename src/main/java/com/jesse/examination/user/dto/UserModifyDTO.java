5262963e89427bca90abc7b1288b92385decbefe
浩进 朱 <3191955858@qq.com>
2025-07-22 18:05:24 +0800

    用户修改自己账户信息时所用的 DTO。

---
+++ src/main/java/com/jesse/examination/user/dto/UserModifyDTO.java
@@ -0,0 +1,30 @@
+package com.jesse.examination.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+import lombok.experimental.Accessors;
+
+/** 用户修改自己账户信息时所用的 DTO。*/
+@Data
+@ToString
+@Accessors(chain = true)
+@EqualsAndHashCode
+@AllArgsConstructor
+public class UserModifyDTO
+{
+    private String oldUserName;            // 旧用户名
+    
+    private String oldPassword;            // 旧密码
+    
+    private String newUserName;            // 新用户名
+
+    private String newPassword;            // 新用户密码
+
+    private String newFullName;            // 新用户全名
+
+    private String newTelephoneNumber;     // 新手机号
+
+    private String newEmail;               // 新邮箱
+}
