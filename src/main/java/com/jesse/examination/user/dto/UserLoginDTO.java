5171b2ca4d8527251965b5ebcf3ad87c8caa1fd5
浩进 朱 <3191955858@qq.com>
2025-07-23 16:56:32 +0800

    用户登录时所用的 DTO。

--- src/main/java/com/jesse/examination/user/dto/UserLoginDTO.java
+++ src/main/java/com/jesse/examination/user/dto/UserLoginDTO.java
@@ -1,17 +1,14 @@
 package com.jesse.examination.user.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.ToString;
+import lombok.*;
 import lombok.experimental.Accessors;
 
 /** 用户登录时所用的 DTO。*/
 @Data
 @ToString
+@NoArgsConstructor
 @Accessors(chain = true)
 @EqualsAndHashCode
-@AllArgsConstructor
 public class UserLoginDTO
 {
     private String userName;
@@ -19,4 +16,13 @@ public class UserLoginDTO
     private String password;
 
     private String verifyCode;
+
+    public static UserLoginDTO
+    of (String name, String pass, String verify)
+    {
+        return new UserLoginDTO()
+            .setUserName(name)
+            .setPassword(pass)
+            .setVerifyCode(verify);
+    }
 }
