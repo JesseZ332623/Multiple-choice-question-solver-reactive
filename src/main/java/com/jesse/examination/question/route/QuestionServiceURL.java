d0171332830e3dfe03a639a0f25a03e53c8d4921
浩进 朱 <3191955858@qq.com>
2025-07-24 18:13:43 +0800

    问题查询模块路由 URL 配置类。

--- src/main/java/com/jesse/examination/question/route/QuestionServiceURL.java
+++ src/main/java/com/jesse/examination/question/route/QuestionServiceURL.java
@@ -1,22 +1,26 @@
 package com.jesse.examination.question.route;
 
-/** 问题查询模块路由 URL 配置类。 */
-public class QuestionServiceURL
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+/** 问题查询模块路由 URL 配置类。*/
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+final public class QuestionServiceURL
 {
     /** 问题相关操作根 URI。*/
     final public static String QUESTION_ROOT_URI
         = "/api/question";
 
     /** 单条问题查询 URI。 */
-    final public static String SINGLE_QUERY_URI
+    final public static String QUESTION_SINGLE_QUERY_URI
         = QUESTION_ROOT_URI + "/single_query";
 
     /** 所有问题完整信息 URI。 */
-    final public static String PAGINATION_QUERY_URI
+    final public static String QUESTION_PAGINATION_QUERY_URI
         = QUESTION_ROOT_URI + "/paginated_query";
 
     /** 所有问题加正确答案信息 URI。 */
-    final public static String PAGINATION_QUERY_WITH_CORRECT_URI
+    final public static String QUESTION_PAGINATION_QUERY_WITH_CORRECT_URI
         = QUESTION_ROOT_URI + "/paginated_query_with_correct";
 
     /** 用户在练习时答对了一道题，这题的答对次数 + 1 URI。*/
