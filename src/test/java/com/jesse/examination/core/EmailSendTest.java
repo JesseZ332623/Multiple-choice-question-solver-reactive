1ac1cb483536755cd0190fad2d7d0a3a1327c8a0
浩进 朱 <3191955858@qq.com>
2025-08-12 14:40:59 +0800

    邮件验证码发送测试类。

--- src/test/java/com/jesse/examination/core/EmailSendTest.java
+++ src/test/java/com/jesse/examination/core/EmailSendTest.java
@@ -1,6 +1,7 @@
 package com.jesse.examination.core;
 
 import com.jesse.examination.core.email.dto.EmailContent;
+import com.jesse.examination.core.email.exception.EmailException;
 import com.jesse.examination.core.email.service.EmailAuthQueryService;
 import com.jesse.examination.core.email.service.EmailSenderInterface;
 import com.jesse.examination.core.email.utils.VerifyCodeGenerator;
@@ -15,8 +16,10 @@ import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
+import java.io.FileNotFoundException;
 import java.time.Duration;
 
+import static com.jesse.examination.core.email.exception.EmailException.ErrorType.ATTACHMENT_NOT_EXIST;
 import static com.jesse.examination.core.redis.keys.ProjectRedisKey.ENTERPRISE_EMAIL_ADDRESS;
 import static com.jesse.examination.core.redis.keys.ProjectRedisKey.SERVICE_AUTH_CODE;
 import static com.jesse.examination.core.logmakers.LogMakers.EMAIL_SENDER;
@@ -70,14 +73,14 @@ public class EmailSendTest
     }
 
     @Test
-    public void EmailSenderTest()
+    public void VerifyCodeEmailSenderTest()
     {
         Mono<Void> sendEmailStream
             = VerifyCodeGenerator.generateVerifyCode(
                 Integer.parseInt(this.projectProperties.getVarifyCodeLength()))
             .flatMap((code) -> {
-                final String userName  = "Jesse";
-                final String userEmail = "zhj3191955858@gmail.com";
+                final String userName  = "Peter-Griffin";
+                final String userEmail = "zhuhaojin@139erp.com";
                 // 尝试发送邮件
                 return this.emailSender
                            .sendEmail(EmailContent.fromVarify(
@@ -98,5 +101,55 @@ public class EmailSendTest
         );
 
         StepVerifier.create(sendEmailStream).verifyComplete();
+    }
+
+    @Test
+    public void AttachmentEmailSenderTest()
+    {
+        Mono<Void> sendEmailStream
+            = Mono.defer(() -> {
+                final String userName  = "Peter-Griffin";
+                final String userEmail = "zhuhaojin@139erp.com";
+
+            try
+            {
+                return
+                this.emailSender
+                    .sendEmail(
+                        EmailContent
+                            .formWithAttachment(
+                                userName, userEmail,
+                                "出生皮特！温州江南皮革厂倒闭了！",
+                                "E:\\图片素材\\Family-Guy Avatar\\Perter 头像.png"
+                            )
+                    )
+                    .doOnSuccess((res) ->
+                        log.info(
+                            EMAIL_SENDER,
+                            "Send email with attachment to {}, {} success!",
+                            userName, userEmail
+                        )
+                    )
+                    .timeout(Duration.ofSeconds(10L));
+            }
+            catch (FileNotFoundException e) {
+                return Mono.error(e);
+            }
+        })
+        .onErrorResume(
+            FileNotFoundException.class,
+            (exception) -> {
+                log.error("{}", exception.getMessage(), exception);
+                return Mono.error(
+                    new EmailException(
+                        ATTACHMENT_NOT_EXIST,
+                        exception.getMessage(), exception
+                    )
+                );
+            }
+        );
+
+        StepVerifier.create(sendEmailStream)
+                    .verifyComplete();
     }
 }
